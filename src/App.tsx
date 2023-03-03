119298af0154412c3798238e5daf1dd9b7b0b359
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-03-03 17:07:43 +0900

    Recoil Selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState); // minutes = 데이터 저장, setMinutes = 데이터 수정
+  const hours = useRecoilValue(hourSelector); // useRecoilValue를 이용하여 hourSelector 값 불러오기
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value); // +를 사용함으로써 string데이터를 number형 데이터로 변환
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/atoms.tsx
@@ -0,0 +1,17 @@
+import { atom, selector } from "recoil";
+
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
+});
+
+export const hourSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    // selector의 get함수는 첫 번쨰 argument로 option이라는 object를 가진다.
+    // 중괄호로 option을 열면 get이라는 함수를 다시 불러 state를 가져올 수 있다.
+    // 개발자가 어떤 값을 return하던지 그 값은 hourSelector의 값이 된다.
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+});
